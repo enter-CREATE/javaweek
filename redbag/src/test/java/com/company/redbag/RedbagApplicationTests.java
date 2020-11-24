cfd17d8c42c9ff3ccc2399222a72228fb233d8fe
Enter <1341503952@qq.com>
2020-11-24 14:22:27 +0800

    22

--- redbag/src/test/java/com/company/redbag/RedbagApplicationTests.java
+++ redbag/src/test/java/com/company/redbag/RedbagApplicationTests.java
@@ -1,13 +1,13 @@
 package com.company.redbag;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RedbagApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
