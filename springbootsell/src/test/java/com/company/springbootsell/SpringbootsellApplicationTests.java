9e05ad48298e08a2744959cbc1370d181f36f4ba
Enter <1341503952@qq.com>
2020-11-26 16:10:19 +0800

    1

--- springbootsell/src/test/java/com/company/springbootsell/SpringbootsellApplicationTests.java
+++ springbootsell/src/test/java/com/company/springbootsell/SpringbootsellApplicationTests.java
@@ -1,13 +1,8 @@
 package com.company.springbootsell;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootsellApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
--- springbootsell/src/test/java/com/company/springbootsell/repository/OrderDetailRepositoryTests.java
+++ springbootsell/src/test/java/com/company/springbootsell/repository/OrderDetailRepositoryTests.java
@@ -1,4 +1,5 @@
 package com.company.springbootsell.repository;
 
 public class OrderDetailRepositoryTests {
+
 }
