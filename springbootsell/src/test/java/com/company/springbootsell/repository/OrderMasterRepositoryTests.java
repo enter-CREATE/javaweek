868660759325c57461fa06fda8a09e9be64764e3
Enter <1341503952@qq.com>
2020-11-26 16:52:06 +0800

    2

--- springbootsell/src/test/java/com/company/springbootsell/repository/OrderMasterRepositoryTests.java
+++ springbootsell/src/test/java/com/company/springbootsell/repository/OrderMasterRepositoryTests.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
