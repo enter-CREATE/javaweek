8be76f93addcdb9d4a5217383cce690a57a110d3
Enter <1341503952@qq.com>
2020-11-30 10:04:46 +0800

    111

---
+++ springbootsell/src/main/java/com/company/springbootsell/Controller/SellerCategoryController.java
@@ -0,0 +1,27 @@
+package com.company.springbootsell.Controller;
+
+import com.company.springbootsell.dataobject.ProductCategory;
+import com.company.springbootsell.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/seller/category")
+public class SellerCategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/list")
+    public ModelAndView list(Map<String ,Object> map){
+        List<ProductCategory> list=categoryService.findAll();
+        map.put("categoryList",list);
+        return new ModelAndView("category/listDeath",map);
+
+    }
+}
