cd3379b689f58c765a30fc340874aee76b78b33b
Enter <1341503952@qq.com>
2021-03-09 16:18:10 +0800

    1

---
+++ delicious/src/test/java/com/example/delicious/DeliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.delicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
